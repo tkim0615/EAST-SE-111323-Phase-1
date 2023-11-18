845b00c25b8ec29b194164663b39c955483ecf50
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-18 10:28:54 -0500

    Addes deliverable solutions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // write your code here
+const greeting = 'Welcome to Flatburger!'
+let num1 = 922
+let num2 = 12399123
+let sum = num1 + num2
+let sumString = `${num1} + ${num2} = ${sum}`
+console.log(sumString)
+
+if(num1 === 7 || num1 === 49){
+    console.log("That's a lucky numer!")} else{
+        console.log('That\'s not a lucky Number.')
+    }
+
